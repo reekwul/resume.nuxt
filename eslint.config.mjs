4e787160520dc2cd9e0d5706776388c76170aeac
reekwul <g.besscetnov@yandex.ru>
2025-09-13 21:46:49 +0300

    linter + prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs';
 
-export default withNuxt();
+export default withNuxt({
+    rules: {
+        'vue/multi-word-component-names': 'off',
+        'vue/no-unused-components': 'error',
+        'no-unused-vars': 'error',
+        'no-console': 'warn',
+        'no-debugger': 'error',
+    },
+});
 // Your custom configs here
